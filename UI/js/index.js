fd70aa4358e64d95677e293845c861f328d9b0cd
Victor-Ugwueze <victorugwueze@gmail.com>
2018-07-09 15:04:28 +0100

    login and signup
    finish tab switching of login and signup feature
     * [ft-#158886013]
    * [ft-#158886014]

--- UI/js/index.js
+++ UI/js/index.js
@@ -24,6 +24,36 @@ const toggleLoginForm = {
         }
 }
 
+
+toggleLoginForm.init();
+
+(function showLoginModal(){
+    [...document.querySelectorAll('.nav-link')].forEach((element)=>{
+        element.addEventListener('click',function(){
+           document.querySelector('#'+this.dataset.target).style.display = 'block';
+        const header =  document.querySelector('header');
+        const main = document.querySelector('main')
+           header.classList.add('active-modal-background');
+           main.classList.add('active-modal-background');
+    
+    
+           const elements = document.querySelectorAll('.modal .tab-pane');
+           let elementToshow = document.querySelector("#"+this.dataset.toggle);
+           SelectElement(elementToshow,[...elements],'selected');
+    
+           
+           elementToshow = document.querySelector('.modal #select-tab-'+this.dataset.toggle);
+           const hideFormElement = document.querySelector('.modal .active');
+    
+           SelectElement(elementToshow,[hideFormElement],'active');
+    
+        });
+    })
+})();
+
+
+
+
 /*
 Remove class from the current element and add class to another element
  @param  Element to add class
@@ -31,11 +61,10 @@ Remove class from the current element and add class to another element
  @param className to add or remove
 */
 function SelectElement(toAddClass,toRemoveclass,className){
-    toRemoveclass.length<2 ? toRemoveclass[0].classList.toggle(className) :
+    toRemoveclass.length<2 ? toRemoveclass[0].classList.remove(className) :
     toRemoveclass.forEach((element)=>{
         if(element.classList.contains(className)) element.classList.remove(className)
         return;
     });
-    toAddClass.classList.toggle(className);
+    toAddClass.classList.add(className);
 }
-toggleLoginForm.init();
