770af5bc641874512ace5440c925bb500547af70
Victor-Ugwueze <victorugwueze@gmail.com>
2018-07-16 23:18:58 +0100

    changed entry

--- models/entry.js
+++ models/entry.js
@@ -1,8 +1,35 @@
+class Entry {
+  contructor(id, title, body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+    this.created_at = new Date();
+  }
+
+  setEntry(id, title, body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+    this.created_at = new Date();
+  }
+
+  getEntry() {
+    return this;
+  }
+}
+
 const entries = [
   {
     id: 1,
-    user_id: 1,
-    title: 'Why I am writing',
+    title: 'Why I am writting',
+    body: 'The reason I am writing this is so that',
+    created_at: '12/7/2018',
+  },
+  {
+    id: 2,
+    title: 'There is joy  in writing',
+    body: 'People write to share and explain their mind',
+    created_at: '14/7/2018',
   },
 ];
 
